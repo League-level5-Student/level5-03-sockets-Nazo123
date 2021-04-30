39f8c5d0993b2552feb320a83abfdfa1828d49e6
Nazo123 <48037675+Nazo123@users.noreply.github.com>
2021-04-29 17:51:16 -0700

    Finished fixes

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -17,6 +17,7 @@ import java.net.UnknownHostException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -44,18 +45,25 @@ public class ChatApp implements ActionListener {
 						Integer.parseInt(JOptionPane.showInputDialog("What port do you want to host this server?")),
 						100);
 				server.setSoTimeout(200000000);
-				JOptionPane.showMessageDialog(null,
-						"Your server is fully set up at the ip " + InetAddress.getLocalHost().getHostAddress()
-								+ " and at port number " + server.getLocalPort() + ".");
+				JFrame frame = new JFrame("Server");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+				JPanel panel = new JPanel();
+				JLabel label = new JLabel("Your server is fully set up at the ip " + InetAddress.getLocalHost().getHostAddress()
+						+ " and at port number " + server.getLocalPort() + ". Waiting for clinet to connect");
+				panel.add(label);
+				frame.add(panel);
+				frame.pack();
 				Socket connection = server.accept();
+				
 				System.out.println("YAY");
 				DataOutputStream os = new DataOutputStream(connection.getOutputStream());
 				DataInputStream is = new DataInputStream(connection.getInputStream());
-				JFrame frame = new JFrame("Server");
-				JPanel panel = new JPanel();
+				panel.remove(label);
 				JTextField field = new JTextField(20);
 				JButton button = new JButton("Send");
 				JTextArea area = new JTextArea();
+				area.setEditable(false);
 				button.addActionListener(this);
 				panel.add(field);
 				panel.add(button);
@@ -64,21 +72,28 @@ public class ChatApp implements ActionListener {
 				frame.pack();
 				frame.setVisible(true);
 				while (connection.isConnected()) {
-					if (shouldSend) {
+					if (shouldSend&!(field.getText().equals(""))) {
 						os.writeUTF(field.getText());
-						area.append("\n You said:"+field.getText());
+						area.append("\n You said: "+field.getText());
 						field.setText("");		
 						shouldSend = false;
 						frame.pack();
 					}
+					else {
+						shouldSend = false;
+					}
 					
 					if(is.available()>0) {
-					area.append("\n Client said:"+is.readUTF());
+					area.append("\n Client said: "+is.readUTF());
 					frame.pack();
 					}
 					}
 			} catch (Exception e) {
 				JOptionPane.showMessageDialog(null, "Failed to connect or lost connection");
+				System.exit(0);
+			}
+			finally{
+				JOptionPane.showMessageDialog(null, "Your connection was lost!");
 				System.exit(0);
 			}
 		} else if (typeDecider == JOptionPane.NO_OPTION) {
@@ -90,10 +105,12 @@ public class ChatApp implements ActionListener {
 				DataOutputStream osc = new DataOutputStream(client.getOutputStream());
 				DataInputStream isc = new DataInputStream(client.getInputStream());
 				JFrame frame = new JFrame("Client");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				JPanel panel = new JPanel();
 				JTextField field = new JTextField(20);
 				JButton button = new JButton("Send");
 				JTextArea area = new JTextArea();
+				area.setEditable(false);
 				button.addActionListener(this);
 				panel.add(field);
 				panel.add(button);
@@ -102,15 +119,18 @@ public class ChatApp implements ActionListener {
 				frame.pack();
 				frame.setVisible(true);
 				while (client.isConnected()) {
-					if (shouldSend) {
+					if (shouldSend&!(field.getText().equals(""))) {
 						osc.writeUTF(field.getText());
-						area.append("\n You said:"+field.getText());
+						area.append("\n You said: "+field.getText());
 						field.setText("");		
 						shouldSend = false;
 						frame.pack();
 					}
+					else {
+						shouldSend = false;
+					}
 					if(isc.available()>0) {
-						area.append("\n Server said:"+isc.readUTF());
+						area.append("\n Server said: "+isc.readUTF());
 						frame.pack();
 						}
 					}
@@ -118,6 +138,10 @@ public class ChatApp implements ActionListener {
 				JOptionPane.showMessageDialog(null, "Failed to connect or lost connection");
 				System.exit(0);
 			} // 192.168.86.244
+			finally{
+				JOptionPane.showMessageDialog(null, "Your connection was lost!");
+				System.exit(0);
+			}
 		}
 	}
 
